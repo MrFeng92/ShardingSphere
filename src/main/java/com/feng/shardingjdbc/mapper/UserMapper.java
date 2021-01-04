cc7d8f9c93346245ca943c2bbcaf27c29410bc19
MrFeng <1406840277@qq.com>
2021-01-04 20:34:14 +0800

    ShardingJDBC

---
+++ src/main/java/com/feng/shardingjdbc/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.feng.shardingjdbc.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.feng.shardingjdbc.entity.Course;
+import com.feng.shardingjdbc.entity.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserMapper extends BaseMapper<User>
+{
+
+}
--- src/test/java/com/feng/shardingjdbc/ShardingjdbcApplicationTests.java
+++ src/test/java/com/feng/shardingjdbc/ShardingjdbcApplicationTests.java
@@ -2,7 +2,11 @@ package com.feng.shardingjdbc;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.feng.shardingjdbc.entity.Course;
+import com.feng.shardingjdbc.entity.Udict;
+import com.feng.shardingjdbc.entity.User;
 import com.feng.shardingjdbc.mapper.CourseMapper;
+import com.feng.shardingjdbc.mapper.UdictMapper;
+import com.feng.shardingjdbc.mapper.UserMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +20,82 @@ public class ShardingjdbcApplicationTests
     //注入mapper
     @Autowired
     private CourseMapper courseMapper;
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private UdictMapper udictMapper;
+
+    //======================测试公共表===================
+    //添加操作
+    @Test
+    public void addDict()
+    {
+        Udict udict = new Udict();
+        udict.setUstatus("a");
+        udict.setUvalue("已启用");
+        udictMapper.insert(udict);
+    }
+
+    //删除操作
+    @Test
+    public void deleteDict()
+    {
+        QueryWrapper<Udict> wrapper = new QueryWrapper<>();
+        //设置userid值
+        wrapper.eq("dictid", 552144666074546177L);
+        udictMapper.delete(wrapper);
+    }
+
+    //======================测试垂直分库==================
+    //添加操作
+    @Test
+    public void addUserDb()
+    {
+        User user = new User();
+        user.setUsername("Jack");
+        user.setUstatus("b");
+        userMapper.insert(user);
+    }
+
+    //查询操作
+    @Test
+    public void findUserDb()
+    {
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        //设置userid值
+        wrapper.eq("user_id", 552283596010815489L);
+        User user = userMapper.selectOne(wrapper);
+        System.out.println(user);
+    }
+
+
+    //=======================测试水平分库===================
+    //添加课程的方法
+    @Test
+    public void addCourseDb()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            Course course = new Course();
+            course.setCname("java" + i);
+            course.setUserId(100L);
+            course.setCstatus("Normal" + i);
+            courseMapper.insert(course);
+        }
+    }
+
+    //查询操作
+    @Test
+    public void findCourseDb()
+    {
+        QueryWrapper<Course> wrapper = new QueryWrapper<>();
+        //设置userid值
+        wrapper.eq("user_id", 100L);
+        //设置cid值L
+        wrapper.eq("cid", 552108566027173889L);
+        Course course = courseMapper.selectOne(wrapper);
+        System.out.println(course);
+    }
 
     //=======================测试水平分表===================
     //添加课程的方法
@@ -26,7 +106,7 @@ public class ShardingjdbcApplicationTests
         {
             Course course = new Course();
             course.setCname("java" + i);
-            course.setUserId(100L);
+            course.setUserId(101L);
             course.setCstatus("Normal" + i);
             courseMapper.insert(course);
         }
